be5eb35b9d5911e78d30f3e48976cc03cb348181
jpedram <jpedram@amazon.com>
2023-08-29 13:43:39 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import axios from 'axios';
+import "@aws-amplify/ui-react/styles.css"; // default theme
+// import awsconfig from './aws-exports';
+// Amplify.configure(awsconfig);
 function App() {
+  const [comment, setComment] = useState('');
+  const [isCommentApproved, setIsCommentApproved] = useState(null);
+
+  const handleInputChange = (e) => {
+    setComment(e.target.value);
+  };
+
+  const submitComment = async () => {
+    try {
+      const response = await axios.post('https://your-api-gateway-url/comments', { comment });
+      setIsCommentApproved(response.data.approved);
+    } catch (error) {
+      console.error('An error occurred while submitting the comment:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Video Broadcast</h1>
+      <div>Video Player Here</div>
+      
+      <h2>Submit your comment:</h2>
+      <input type="text" value={comment} onChange={handleInputChange} />
+      <button onClick={submitComment}>Submit</button>
+      
+      {isCommentApproved !== null && (
+        isCommentApproved
+          ? <p>Your comment has been approved!</p>
+          : <p>Your comment was not approved.</p>
+      )}
     </div>
   );
 }
